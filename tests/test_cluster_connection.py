d7041bd8d0472cd5b263abdfecaa6ed859eefc8c
Jinghan11 <linjh@connect.hku.hk>
2024-05-13 00:56:04 +0800

    Add optional param (temperature, num_predict) for generate endpoint

--- tests/test_cluster_connection.py
+++ tests/test_cluster_connection.py
@@ -4,12 +4,15 @@ from ollama import Client
 # client = Client(host='http://10.64.86.xxx:3060')
 
 # cluster RTX 3090
-client = Client(host='http://10.64.86.xxx:3060')
+# client = Client(host='http://10.64.86.xxx:3060')
 
 # cluster RTX 4060Ti
 # client = Client(host='http://10.64.193.xxx:3060')
 
-# response = client.generate(model='llama3', messages=[
+# cluster RTX 4060Ti
+client = Client(host='http://172.17.0.1:3060')
+
+# response = client.chat(model='llama3', messages=[
 #   {
 #     'role': 'user',
 #     'content': 'Why is the sky blue?',
@@ -17,7 +20,8 @@ client = Client(host='http://10.64.86.xxx:3060')
 # ])
 
 response = client.generate(model='llama3', prompt=
-    'Why is the sky blue?',
+    'Why is the sky blue?', options={'temperature':0,
+                                     'num_predict':500,}
 )
 
 print(f"{response['response']}\nResponse Speed is  {(response['eval_count'] / response['eval_duration']) * 10**9} token/sec")
