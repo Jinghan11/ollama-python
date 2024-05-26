fd4c4e512a6d6cc382a9ccd8376568bfa5cdb33c
Jinghan11 <linjh@connect.hku.hk>
2024-05-27 02:21:03 +0800

    fix running rate bug of callback function

--- tests/test_multimodel_response.py
+++ tests/test_multimodel_response.py
@@ -7,48 +7,58 @@ import time  # Import time to measure response times
 
 
 def image_callback(msg):
-    # transform the image to cv2 format
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    # Static variable to store the last time the image was processed
+    if not hasattr(image_callback, "last_time"):
+        image_callback.last_time = 0
+
+    current_time = time.time()
     
-    # image preprocessing
-    resized_image = cv2.resize(cv_image, (224, 224))
-    
-    # send the image to the multimodel
-    _, img_encoded = cv2.imencode('.jpg', resized_image)
-    img_bytes = img_encoded.tobytes()
+    # Process at certain frequency
+    if current_time - image_callback.last_time >= 1.5:
 
-    client = Client(host='http://10.64.86.215:3060')
+        # Record the timestamp before sending the request
+        start_time = time.time()
 
-    # Record the timestamp before sending the request
-    start_time = time.time()
+        # transform the image to cv2 format
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        
+        # image preprocessing
+        resized_image = cv2.resize(cv_image, (224, 224))
+        
+        # Optionally display the resized image
+        cv2.imshow("Resized Image", resized_image)
+        cv2.waitKey(1)
 
-    response = client.generate(model='llava', prompt=
-    'The figure are from gazebo sim env, describe the objeuct in fig in a sentence:', images=[img_bytes], options={'temperature':0,
-                                     'num_predict':500,}
-)
-    
-    # Record the timestamp after receiving the response
-    end_time = time.time()
+        # send the image to the multimodel
+        _, img_encoded = cv2.imencode('.jpg', resized_image)
+        img_bytes = img_encoded.tobytes()
 
-    # Calculate the response time in seconds
-    response_time = end_time - start_time
+        client = Client(host='http://192.168.1.113:3060')
 
-    # print(response['response'], end='\n\n', flush=True)
-    # print(f"{response['response']}\nResponse Speed is  {(response['eval_count'] / response['eval_duration']) * 10**9} token/sec")
-    print(f"Response: {response['response']}")
-    print(f"Response Time: {response_time:.3f} seconds")
+        response = client.generate(model='llava', prompt=
+        'The figure are from gazebo sim env, describe the objeuct in fig in a sentence:', images=[img_bytes], options={'temperature':0,
+                                        'num_predict':500,}
+    )
+        
+        # Record the timestamp after receiving the response
+        end_time = time.time()
+
+        # Calculate the response time in seconds
+        response_time = end_time - start_time
+
+        # print(response['response'], end='\n\n', flush=True)
+        # print(f"{response['response']}\nResponse Speed is  {(response['eval_count'] / response['eval_duration']) * 10**9} token/sec")
+        print(f"Response: {response['response']}")
+        print(f"Response Time: {response_time:.3f} seconds")
+
+        image_callback.last_time = current_time
 
 
 def main():
     rospy.init_node('llava_node', anonymous=True)
-    rospy.Subscriber('/robot2/camera_d435/color/image_raw', Image, image_callback)
-    
-    # set the rate of the node
-    rate = rospy.Rate(0.5) 
-    
-    while not rospy.is_shutdown():
-        rate.sleep()
+    rospy.Subscriber('/robot1/camera_d435/color/image_raw', Image, image_callback, queue_size=1)
+    rospy.spin()
 
 if __name__ == '__main__':
     main()
